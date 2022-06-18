420edbc576c11777ff177e5253ff91e3af13c615
AventureCloud <93791265+AventureCloud@users.noreply.github.com>
2022-06-17 21:57:49 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 yum install -y httpd php git
 service httpd start
+sudo systemctl enable httpd.service
 cd
 git clone https://github.com/AventureCloud/Centos-Bootstrap
 cp ./Centos-Bootstrap/* /var/www/html
 mv /var/www/html/htaccess /var/www/html/.htaccess
+sudo systemctl restart httpd.service
